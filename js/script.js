d6d185c7f6c876eb47072a0cfe1fe6449965a988
Dmytro Havrylov <havrylov_d@ukr.net>
2022-09-22 17:00:00 +0300

    commit initial

---
+++ js/script.js
@@ -0,0 +1,31 @@
+"use strict"; //used new version JS
+
+a=15;
+console.log(a);
+
+let number = 5; //change
+const leftBorderWidth = 1; //like constant, but not
+
+number = 10;
+
+console.log(number);
+/* 
+---------------------------------------
+*/
+const obj = {
+    a: 50  
+};
+
+obj.a=15;
+
+console.log(obj.a);
+/* 
+---------------------------------------
+*/
+console.log(name);
+var name = 'Ivan';
+
+name = 'Alex';
+
+
+console.log(name);
